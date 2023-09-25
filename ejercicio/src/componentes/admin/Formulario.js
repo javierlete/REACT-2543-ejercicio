6f0b68e36c86356645a4d977e4275afeeb6e25fd
Javier <javier@eskura21.com>
2023-09-25 19:05:44 +0200

    Enlazamiento de datos de producto con estado

--- ejercicio/src/componentes/admin/Formulario.js
+++ ejercicio/src/componentes/admin/Formulario.js
@@ -1,12 +1,12 @@
 import LabelInput from "./LabelInput";
 
-export default function Formulario({ producto }) {
+export default function Formulario({ producto, setProducto }) {
     return (
         <form>
-            <LabelInput nombre="Id" id="id" tipo="number" requerido={false} valor={producto.id} />
-            <LabelInput nombre="Nombre" id="nombre" tipo="text" textoMinimo="3" requerido={true} valor={producto.nombre} />
-            <LabelInput nombre="Precio" id="precio" tipo="number" numeroMinimo="0" requerido={true} valor={producto.precio} />
-            <LabelInput nombre="Descripción" id="descripcion" tipo="textarea" valor={producto.descripcion} />
+            <LabelInput nombre="Id" id="id" tipo="number" requerido={false} soloLectura={true} valor={producto.id} />
+            <LabelInput nombre="Nombre" id="nombre" tipo="text" textoMinimo="3" requerido={true} valor={producto.nombre} modificarValor={valor => setProducto({ ...producto, nombre: valor })} />
+            <LabelInput nombre="Precio" id="precio" tipo="number" numeroMinimo="0" requerido={true} valor={producto.precio} modificarValor={valor => setProducto({ ...producto, precio: valor })}/>
+            <LabelInput nombre="Descripción" id="descripcion" tipo="textarea" valor={producto.descripcion} modificarValor={valor => setProducto({ ...producto, descripcion: valor })} />
             <LabelInput nombre="Guardar" tipo="button" />
         </form>
     );
--- ejercicio/src/componentes/admin/LabelInput.js
+++ ejercicio/src/componentes/admin/LabelInput.js
@@ -1,15 +1,16 @@
-export default function LabelInput({ nombre, id, tipo, requerido, textoMinimo, numeroMinimo, valor, onEnviar }) {
+export default function LabelInput({ nombre, id, tipo, requerido, soloLectura, textoMinimo, numeroMinimo, valor, onEnviar, modificarValor }) {
     let control;
 
     switch(tipo) {
         case 'textarea':
-            control = <textarea required={requerido} minLength={textoMinimo} min={numeroMinimo} className="form-control" id={id} value={valor}/>;
+            control = <textarea required={requerido} minLength={textoMinimo} min={numeroMinimo} className="form-control" id={id} value={valor} onChange={e => modificarValor(e.target.value)} />;
             break;
         case 'button':
             control = <button onClick={onEnviar} type="submit" className="btn btn-primary">{nombre}</button>;
             break;
         default:
-            control = <input type={tipo} required={requerido} minLength={textoMinimo} min={numeroMinimo} className="form-control" id={id} value={valor} />;
+            control = <input type={tipo} required={requerido} readOnly={soloLectura} minLength={textoMinimo} min={numeroMinimo} className="form-control" id={id} value={valor} onChange={e => modificarValor(e.target.value)} />;
+            
     }
     return (
         <div className="row mb-3">
