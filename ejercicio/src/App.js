e81c76ee6497acd79d1d22445c8f8516d1fb0b63
Javier <javier@eskura21.com>
2023-09-25 20:06:56 +0200

    Obtención de productos de fichero JSON

--- ejercicio/src/App.js
+++ ejercicio/src/App.js
@@ -4,19 +4,27 @@ import Fichas from './componentes/Fichas';
 import Menu from './componentes/Menu';
 import Pie from './componentes/Pie';
 
-import { PRODUCTOS } from './constantes/productos';
 import { MENUS } from './constantes/menus';
 import { ANUNCIOS } from './constantes/anuncios';
 import { REDES } from './constantes/redes';
+import { useEffect, useState } from 'react';
 
 
 export default function App() {
+  const [productos, setProductos] = useState([]);
+  
+  useEffect(() => {
+    fetch('json/productos.json')
+      .then(respuesta => respuesta.json())
+      .then(productos => setProductos(productos));
+  }, []);
+
   return (
     <>
       <Menu menus={MENUS} />
       <main className="container">
         {/* <Fichas productos={PRODUCTOS} /> */}
-        <Admin productos={PRODUCTOS} />
+        <Admin productos={productos} />
       </main>
       {/* <Anuncios anuncios={ANUNCIOS} /> */}
       <Pie redes={ REDES } />
